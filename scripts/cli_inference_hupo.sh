5ab4dcba9cf034477a73d09f4a9a489480fa2017
hu.po <hugo.p.cmu@gmail.com>
2023-10-11 06:48:58 -0500

    with image

--- scripts/cli_inference_hupo.sh
+++ scripts/cli_inference_hupo.sh
@@ -1,4 +1,4 @@
 python -m llava.serve.cli \
-    --model-path liuhaotian/llava-v1.5-7b \
-    --image-file "https://llava-vl.github.io/static/images/view.jpg" \
+    --model-path /home/oop/dev/LLaVA/llava-v1.5-13b \
+    --image-file /home/oop/dev/LLaVA/scripts/test_image_pokemon.png \
     --load-4bit
