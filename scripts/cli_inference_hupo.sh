3844de4a3c7192504c4e1d84edd9525f606a7119
hu.po <hugo.p.cmu@gmail.com>
2023-10-14 05:34:40 -0500

    test images

--- scripts/cli_inference_hupo.sh
+++ scripts/cli_inference_hupo.sh
@@ -1,4 +1,4 @@
 python -m llava.serve.cli \
     --model-path /home/oop/dev/LLaVA/llava-v1.5-13b \
-    --image-file /home/oop/dev/LLaVA/scripts/test_image_pokemon.png \
+    --image-file /home/oop/dev/LLaVA/images/test_rockbama.png \
     --load-4bit
